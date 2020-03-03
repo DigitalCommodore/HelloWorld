0607c9f37a589a27811adab20c4cd4b991b0897e
Dallas.DeMarr <Dallas.DeMarr@turbonomic.com>
2020-03-03 16:21:06 -0700

    Progress

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Age?");
+            var age = Console.ReadLine();
+            Console.WriteLine("Birth Month?");
+            var month = Console.ReadLine();
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Birth Month: {0}", month);
         }
     }
 }
